1192c7aa3cb5d2a46df4c7620cc431cb025b3f12
k.sureshkumar <92170229+kskarumakula2345@users.noreply.github.com>
2022-04-26 19:39:25 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -2,7 +2,7 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
-       sh "mvn clean package"
+       powershell "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
